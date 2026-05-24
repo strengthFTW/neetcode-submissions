1c60c79d8b5f220e8078ac465d8be5bc1bf86dce
Ritesh <riteshpashikanti321@gmail.com>
2026-05-24 13:41:24 +0530

    Add: duplicate-integer - submission-1

---
+++ Data Structures & Algorithms/duplicate-integer/submission-1.java
@@ -0,0 +1,14 @@
+class Solution {
+    public boolean hasDuplicate(int[] nums) {
+        HashMap<Integer,Integer> map = new HashMap<>();
+        for(int i=0 ;i<nums.length; i++) {
+            map.put(nums[i],map.getOrDefault(nums[i],0)+1);
+        }
+        for(int val : map.values()) {
+            if(val > 1) {
+                return true;
+            }
+        }
+        return false;
+    }
+}
